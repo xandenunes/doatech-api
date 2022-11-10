f43d681ba26fea99a5df674b3c754e60637fc5a0
SOLUTIS\alexandre.brito <alexandre.brito@solutis.com.br>
2022-11-10 18:50:01 -0300

    first commit

---
+++ src/test/java/com/example/doatech/DoatechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoatechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
